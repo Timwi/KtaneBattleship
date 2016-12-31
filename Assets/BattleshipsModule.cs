986025ccbd603b030d06bf557a24ea709cc45252
Timwi <github@timwi.de>
2016-12-31 14:46:59 +0100

    Blank project.

---
+++ Assets/BattleshipsModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Battleships;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Battleships
+/// Created by Timwi
+/// </summary>
+public class BattleshipsModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[Battleships] Started");
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Battleships] Activated");
+    }
+}
